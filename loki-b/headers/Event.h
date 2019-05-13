386fa6c0579e103406fd41718fb5ca9e3f1b7284
DAANBOER <daan-boer@hotmail.com>
2019-05-13 12:26:02 +0100

    Added event-listener system to main project

---
+++ loki-b/headers/Event.h
@@ -0,0 +1,41 @@
+//
+// Created by daan on 13-5-19.
+//
+
+#ifndef LOKI_CPP_EVENT_H
+#define LOKI_CPP_EVENT_H
+
+#include <vector>
+#include <functional>
+
+namespace loki {
+    template<typename ...T>
+    class Event {
+        std::vector<std::function<void(T...)>> callbacks;
+
+    public:
+        Event() = default;
+
+        Event(const Event &other) = delete;
+
+        ~Event() = default;
+
+        void emit(T ...Args) {
+            for (const auto &callback : callbacks)
+                callback(Args...);
+        }
+
+        void addListener(std::function<void(T...)> f) {
+            callbacks.emplace_back(f);
+        }
+
+        template<class C>
+        void addListener(void (C::*f)(T... Args), C &c) {
+            callbacks.emplace_back([&c, f](T... t) -> void { (c.*f)(t...); });
+        }
+    };
+
+    using event = Event<>;
+}
+
+#endif //LOKI_CPP_EVENT_H
