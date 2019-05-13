f8e73093fec19982b5944b969d40bd9da0cc51bb
DAANBOER <daan-boer@hotmail.com>
2019-05-13 17:30:01 +0100

    added tests for range parsing

--- tests/loki_tests/main.cpp
+++ tests/loki_tests/main.cpp
@@ -11,16 +11,17 @@ using std::endl;
 using namespace loki;
 
 void parse_tests_phase_one();
+void parse_tests_phase_two();
 
-void notify(const char* message)
-{
+void notify(const char *message) {
     std::cout << message << std::endl;
 }
 
-int main (int argc, char ** argv)
-{
+int main(int argc, char **argv) {
     notify("Testing: Parsing Phase One");
     parse_tests_phase_one();
+    notify("Testing: Parsing Phase Two");
+    parse_tests_phase_two();
 
     notify("All tests complete");
 }
@@ -29,8 +30,7 @@ int main (int argc, char ** argv)
 
 /** PHASE ONE **/
 
-void parse_bool()
-{
+void parse_bool() {
     bool value = false;
     std::string field = "test_value: true";
 //    std::string field_other = "test_value: TRUE";
@@ -42,8 +42,7 @@ void parse_bool()
 //    assert(value);
 }
 
-void parse_double()
-{
+void parse_double() {
     double value = 0;
     std::string field = "test_value: \t  \t\t  42   \n\n ";
 
@@ -54,11 +53,11 @@ void parse_double()
     assert(value == 42.);
 }
 
-void parse_string()
-{
+void parse_string() {
     std::string value;
     std::string field = "    electricQuadrupoleMoment: Databases/quadrupoleMoment.txt";
     std::string field_other = "    electricQuadrupoleMoment: \t \tDatabases/quadrupoleMoment.txt  \n\n\n\n  ";
+    std::string field_range = " electronTemperature: logspace(11.4574, 23.5, 8) ";
     std::string section = "  gasProperties:                       \n"
                           "    mass: Databases/masses.txt\n"
                           "    fraction:\n"
@@ -77,12 +76,14 @@ void parse_string()
     assert(Parse::setField(field_other, "electricQuadrupoleMoment", value));
     assert(value == "Databases/quadrupoleMoment.txt");
 
+    assert(Parse::setField(field_range, "electronTemperature", value));
+    assert(value == "logspace(11.4574, 23.5, 8)");
+
     assert(Parse::setField(section, "electricQuadrupoleMoment", value));
     assert(value == "Databases/quadrupoleMoment.txt");
 }
 
-void parse_vector()
-{
+void parse_vector() {
     std::vector<std::string> vec;
     std::string section = "  isOn: false\n"
                           "  folder: simulation1\n"
@@ -103,8 +104,7 @@ void parse_vector()
     }
 }
 
-void parse_section()
-{
+void parse_section() {
     std::string content = "  gasProperties:                       \n"
                           "    mass: Databases/masses.txt\n"
                           "    fraction:\n"
@@ -132,8 +132,7 @@ void parse_section()
     assert(sectionBuffer == correct_result);
 }
 
-void parse_tests_phase_one()
-{
+void parse_tests_phase_one() {
     test(parse_bool);
     test(parse_double);
     test(parse_string);
@@ -142,3 +141,23 @@ void parse_tests_phase_one()
 }
 
 /** PHASE TWO **/
+
+void parse_value_range() {
+    std::string rString = "logspace(11.4574, 23.5, 8)";
+    std::string rStringOther = "logspace( 748.,23.5,8)";
+    std::string vString = "23.48392";
+    double value = 0;
+
+    assert(Parse::getFirstValue(rString, value));
+    assert(value == 11.4574);
+
+    assert(Parse::getFirstValue(rStringOther, value));
+    assert(value == 748.);
+
+    assert(Parse::getFirstValue(vString, value));
+    assert(value == 23.48392);
+}
+
+void parse_tests_phase_two() {
+    test(parse_value_range);
+}
