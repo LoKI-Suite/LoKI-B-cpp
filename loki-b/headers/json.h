8af8ee0da8b696dca49c5610dd9073cd294cec9a
jan <jan@linux-a9ex>
2020-07-16 22:23:02 +0200

    Support code for the nlohmann JSON library.
     * added the header in include/nlohmann/json.hpp
     * json.h, json.cpp: in namespace loki, introduce typename
       json_type = nlohmann::json, add functions read_json_from_stream,
       read_json_from_file.
     * add json.{h,cpp} to CMakeLists.txt

---
+++ loki-b/headers/json.h
@@ -0,0 +1,17 @@
+#ifndef LOKI_CPP_JSON_H
+#define LOKI_CPP_JSON_H
+
+#include "../include/nlohmann/json.hpp"
+#include <iostream>
+#include <string>
+
+namespace loki {
+
+using json_type = nlohmann::json;
+
+json_type read_json_from_stream(std::istream& is);
+json_type read_json_from_file(const std::string& fname);
+
+}; // namespace loki
+
+#endif // LOKI_CPP_JSON_H
