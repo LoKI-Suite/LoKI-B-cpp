a1b3dd61887eefb610537ec17b592ecca4b7643a
jan <jan@linux-a9ex>
2020-07-23 13:30:30 +0200

    Added a ctor overload that accepts a json object.

--- loki-b/source/WorkingConditions.cpp
+++ loki-b/source/WorkingConditions.cpp
@@ -14,9 +14,12 @@ namespace loki {
     // TODO: Comment on WorkingConditions().
 
     WorkingConditions::WorkingConditions(const WorkingConditionsSetup &setup, const Enumeration::EedfType &eedfType) :
-        gasPressure(setup.gasPressure), gasTemperature(setup.gasTemperature),
-        electronDensity(setup.electronDensity), chamberLength(setup.chamberLength),
-        chamberRadius(setup.chamberRadius), excitationFrequency(setup.excitationFrequency) {
+        gasPressure(setup.gasPressure),
+        gasTemperature(setup.gasTemperature),
+        electronDensity(setup.electronDensity),
+        chamberLength(setup.chamberLength),
+        chamberRadius(setup.chamberRadius),
+        excitationFrequency(setup.excitationFrequency) {
 
         if ((eedfType == EedfType::boltzmann) &&
                 !Parse::getFirstValue(setup.reducedField, reducedField)) {
@@ -25,6 +28,32 @@ namespace loki {
         }
         if ((eedfType == EedfType::prescribed) &&
                 !Parse::getFirstValue(setup.electronTemperature, electronTemperature)) {
+
+            Log<ParseFieldError>::Error("electronTemperature");
+        }
+
+        gasDensity = gasPressure / (Constant::boltzmann * gasTemperature);
+        reducedFieldSI = reducedField * 1.e-21;
+        reducedExcFreqSI = excitationFrequency * 2 * Constant::pi / gasDensity;
+
+        linkToArgumentMap();
+    }
+
+    WorkingConditions::WorkingConditions(const json_type &cnf, const Enumeration::EedfType &eedfType) :
+        gasPressure(cnf.at("gasPressure").get<double>()),
+        gasTemperature(cnf.at("gasTemperature").get<double>()),
+        electronDensity(cnf.at("electronDensity").get<double>()),
+        chamberLength(cnf.at("chamberLength").get<double>()),
+        chamberRadius(cnf.at("chamberRadius").get<double>()),
+        excitationFrequency(cnf.at("excitationFrequency").get<double>()) {
+
+        if ((eedfType == EedfType::boltzmann) &&
+                !Parse::getFirstValue(cnf.at("reducedField"), reducedField)) {
+
+            Log<ParseFieldError>::Error("reducedField");
+        }
+        if ((eedfType == EedfType::prescribed) &&
+                !Parse::getFirstValue(cnf.at("electronTemperature"), electronTemperature)) {
 
             Log<ParseFieldError>::Error("electronTemperature");
         }
