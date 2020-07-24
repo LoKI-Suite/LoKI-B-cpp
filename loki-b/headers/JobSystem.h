35de2ace701d7d5afa2283c3625746bd1b87bb3b
jan <jan@linux-a9ex>
2020-07-24 16:45:12 +0200

    Allow construction with a string or json_type.
    Added constructor overloads that allow Range construction from
    string (legacy) and json_type variables. To this end, getRange
    from Parse has been duplicated in lokib::impl in the cpp file
    for the time being.

--- loki-b/headers/JobSystem.h
+++ loki-b/headers/JobSystem.h
@@ -10,18 +10,27 @@
 #include <vector>
 #include <cmath>
 #include <sstream>
+#include <stdexcept>
+#include "json.h"
 
 namespace loki {
     class WorkingConditions;
 
     class Range {
-        const bool isLog;
-        const double start, stop;
+        /** \todo make isLog, start, stop and n constant after the string and json_type ctors
+         *  have been updated to allow that.
+         */
+        bool isLog;
+        double start, stop;
 
-        const uint32_t n;
+        uint32_t n;
         uint32_t iter{0};
 
     public:
+        // expects a legacy Range string: a numerical value or a linspan or logspan description
+        Range(const std::string& str);
+        Range(const json_type& cnf);
+        Range(double value) : isLog(false), start(value), stop(value), n(1) {}
         Range(double start, double stop, uint32_t nSteps, bool isLog) : isLog(isLog), start(start), stop(stop), n(nSteps) {}
 
         Range(const Range &other) : isLog(other.isLog), start(other.start), stop(other.stop), n(other.n) {}
@@ -30,11 +39,12 @@ namespace loki {
             return (n - 1) > iter++;
         }
 
+        // JvD: also support n=1, a degenerate range with only one value (the start value). Avoid /0.
         double value() const {
             if (isLog)
-                return std::pow(10., start + iter * (stop - start) / (n - 1));
+                return n==1 ? std::pow(10.,start) : std::pow(10., start + iter * (stop - start) / (n - 1));
 
-            return start + iter * (stop - start) / (n - 1);
+            return n==1 ? start : start + iter * (stop - start) / (n - 1);
         }
 
         void reset() {
@@ -71,6 +81,12 @@ namespace loki {
             jobs.emplace_back(job);
         }
 
+        void prepareFirstJob() {
+            for (Job& job : jobs)
+            {
+                (wc->*job.callback)(job.range.value());
+            }
+        }
         bool nextJob() {
             Job &job = jobs[jobIndex];
 
