441548ec910ed7ab2416ff1ac132855339f528d6
jan <jan@linux-a9ex>
2020-07-27 16:52:45 +0200

    Header fixes, out-of-line members, license.
    Include headers only where/when needed. Implement some members
    out-of-line. Added license and copyright information.

--- loki-b/headers/JobSystem.h
+++ loki-b/headers/JobSystem.h
@@ -1,19 +1,37 @@
-//
-// Created by daan on 08-07-2019.
-//
+/** \file LoKI-B support for parameterized series of simulations.
+ *
+ *  LoKI-B solves a time and space independent form of the two-term
+ *  electron Boltzmann equation (EBE), for non-magnetised non-equilibrium
+ *  low-temperature plasmas excited by DC/HF electric fields from
+ *  different gases or gas mixtures.
+ *  Copyright (C) 2018-2020 A. Tejero-del-Caz, V. Guerra, D. Goncalves,
+ *  M. Lino da Silva, L. Marques, N. Pinhao, C. D. Pintassilgo and
+ *  L. L. Alves
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  \author Daan Boer and Jan van Dijk (C++ version)
+ */
 
 #ifndef LOKI_CPP_JOBSYSTEM_H
 #define LOKI_CPP_JOBSYSTEM_H
 
-#include <cstdint>
+#include <cstdlib>
 #include <string>
 #include <vector>
-#include <cmath>
-#include <sstream>
-#include <stdexcept>
 #include <functional>
 #include "json.h"
-#include <cassert>
 
 namespace loki {
 
@@ -51,11 +69,7 @@ namespace loki {
         /// returns the number of values of this Range.
         size_type size() const { return m_size; }
         /// Returns the \a ndx'th value. Argument \a ndx must be smaller than size().
-        double value(size_type ndx) const
-        {
-            assert(ndx<size());
-            return get_value(ndx);
-        }
+        double value(size_type ndx) const;
     protected:
         /// This constructor records \a size, the number of values in the Range
         Range(size_type size) : m_size(size) {}
--- loki-b/source/JobSystem.cpp
+++ loki-b/source/JobSystem.cpp
@@ -1,6 +1,34 @@
-//
-// Created by daan on 08-07-2019.
-//
+/** \file LoKI-B support for parameterized series of simulations.
+ *
+ *  LoKI-B solves a time and space independent form of the two-term
+ *  electron Boltzmann equation (EBE), for non-magnetised non-equilibrium
+ *  low-temperature plasmas excited by DC/HF electric fields from
+ *  different gases or gas mixtures.
+ *  Copyright (C) 2018-2020 A. Tejero-del-Caz, V. Guerra, D. Goncalves,
+ *  M. Lino da Silva, L. Marques, N. Pinhao, C. D. Pintassilgo and
+ *  L. L. Alves
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  \author Daan Boer and Jan van Dijk (C++ version)
+ */
+
+
+#include <cmath>
+#include <sstream>
+#include <stdexcept>
+#include <cassert>
 
 #include "JobSystem.h"
 #include "Log.h"
@@ -10,6 +38,12 @@ namespace loki {
 
 Range::~Range()
 {
+}
+
+double Range::value(size_type ndx) const
+{
+    assert(ndx<size());
+    return get_value(ndx);
 }
 
 /** A degenerate Range implementation that describes a single value.
