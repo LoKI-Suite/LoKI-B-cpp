fb1bc1b1f77452a9657b7027330c35756798b6db
jan <jan@linux-a9ex>
2020-08-08 15:50:31 +0200

    StateContainer: merged with generic State template

--- loki-b/ideas/StateContainer.h
+++ loki-b/ideas/StateContainer.h
@@ -14,6 +14,145 @@
 namespace loki {
 namespace experimental {
 
+//enum StateLevel { Charge, Electronic, Vibrational, Rotational };
+
+template <class ChildT>
+using ChildContainer = std::vector<std::unique_ptr<ChildT>>;
+
+template <typename StateInfo, typename ParentT, typename StateT, typename ChildT>
+class State
+{
+public:
+    using Parent = ParentT;
+    using ChildType = ChildT;
+    using ChildContainer = loki::experimental::ChildContainer<ChildType>;
+
+    using size_type = typename ChildContainer::size_type;
+    using iterator = typename ChildContainer::iterator;
+    using const_iterator = typename ChildContainer::const_iterator;
+
+    State(const ParentT& parent, const std::string& name)
+    : m_parent(parent),
+      m_info(name)
+    {
+        static_assert(std::is_base_of_v<State,StateT>);
+    }
+
+    const Parent& parent() const { return m_parent; }
+    const StateInfo& info() const { return m_info; }
+    // reconsider:
+    StateInfo& info() { return m_info; }
+
+    const ChildContainer& children() const { return m_children; }
+    bool empty() const { return m_children.empty(); }
+    size_type size() const { return m_children.size(); }
+    iterator begin() { return m_children.begin(); }
+    iterator end() { return m_children.end(); }
+    const_iterator begin() const { return m_children.begin(); }
+    const_iterator end() const { return m_children.end(); }
+
+
+    ChildT* add_state(const std::string& info)
+    {
+        return m_children.emplace_back(new ChildType(*static_cast<StateT*>(this),info)).get();
+    }
+    ChildT* ensure_state(const std::string& info)
+    {
+        ChildT* c = find(info);
+        return c ? c : add_state(info);
+    }
+    template <class ...Args>
+    auto ensure_state(const std::string& info, Args... args)
+    {
+        return ensure_state(info)->ensure_state(args...);
+    }
+    bool operator==(const std::string& info) const
+    {
+        return m_info==info;
+    }
+    void print(std::ostream& os, const std::string& prefix=std::string{}) const
+    {
+        os << prefix << m_info.str() << std::endl;
+        for (const auto& c : m_children)
+        {
+            c->print(os,prefix);
+        }
+    }
+    ChildType* find(const std::string& info) const
+    {
+        for (const auto& c : m_children)
+        {
+            if (*c==info)
+            {
+                return c.get();
+            }
+        }
+        return nullptr;
+    }
+    void checkPopulations() const
+    {
+        if (m_children.empty())
+        {
+            return;
+        }
+        double totalPopulation = 0.;
+        for (auto& state : m_children)
+        {
+            totalPopulation += state->info().population;
+            state->checkPopulations();
+        }
+        if (std::abs(totalPopulation - 1.) > 10. * std::numeric_limits<double>::epsilon())
+        {
+            throw std::runtime_error("Populations of children of '" + m_info.str() + "' do not add up to unity.");
+        }
+    }
+private:
+    const Parent& m_parent;
+    ChildContainer m_children;
+    StateInfo m_info;
+};
+
+template <typename StateInfo, typename ParentT, typename StateT>
+class State<StateInfo,ParentT,StateT,void>
+{
+public:
+    using Parent = ParentT;
+
+    const Parent& parent() const { return m_parent; }
+    const StateInfo& info() const { return m_info; }
+    // reconsider:
+    StateInfo& info() { return m_info; }
+
+    bool operator==(const std::string& info) const
+    {
+        return m_info==info;
+    }
+    void print(std::ostream& os, const std::string& prefix=std::string{}) const
+    {
+        os << prefix << m_info.str() << std::endl;
+    }
+    State(const ParentT& parent, const std::string& name)
+    : m_parent(parent),
+      m_info(name)
+    {
+        static_assert(std::is_base_of_v<State,StateT>);
+    }
+    void checkPopulations() const
+    {
+    }
+private:
+    const Parent& m_parent;
+    StateInfo m_info;
+};
+
+#if 0
+template <class StateT>
+const typename StateT::Parent::ChildContainer& siblings(const StateT& state)
+{
+    return state.parent().children();
+}
+#endif
+
 #if 0
 
 /// \todo Use this later, instead of the ad-hoc members below (Info base class or member)?
@@ -116,134 +255,6 @@ private:
 };
 
 
-//enum StateLevel { Charge, Electronic, Vibrational, Rotational };
-
-template <typename ChildT>
-class ChildContainer
-{
-    using ChildrenType = std::vector<std::unique_ptr<ChildT>>;
-public:
-    using ChildType = ChildT;
-    using size_type = typename ChildrenType::size_type;
-    using iterator = typename ChildrenType::iterator;
-    using const_iterator = typename ChildrenType::const_iterator;
-
-    bool empty() const { return m_children.empty(); }
-    size_type size() const { return m_children.size(); }
-    iterator begin() { return m_children.begin(); }
-    iterator end() { return m_children.end(); }
-    const_iterator begin() const { return m_children.begin(); }
-    const_iterator end() const { return m_children.end(); }
-
-    void print(std::ostream& os, const std::string& prefix=std::string{}) const
-    {
-        for (const auto& c : m_children)
-        {
-            c->print(os,prefix);
-        }
-    }
-    template <class StateT>
-    ChildT* add(StateT& parent, const std::string& info)
-    {
-        return m_children.emplace_back(new ChildType(parent,info)).get();
-    }
-    ChildType* find(const std::string& info) const
-    {
-        for (const auto& c : m_children)
-        {
-            if (*c==info)
-            {
-                return c.get();
-            }
-        }
-        return nullptr;
-    }
-    void checkPopulations(const std::string& name) const
-    {
-        if (m_children.empty())
-        {
-            return;
-        }
-        double totalPopulation = 0.;
-        for (auto& state : m_children)
-        {
-            totalPopulation += state->info().population;
-            state->checkPopulations();
-        }
-        if (std::abs(totalPopulation - 1.) > 10. * std::numeric_limits<double>::epsilon())
-        {
-            throw std::runtime_error("Populations of children of '" + name + "' do not add up to unity.");
-        }
-    }
-private:
-    ChildrenType m_children;
-};
-
-template <>
-class ChildContainer<void>
-{
-public:
-    void print(std::ostream& os, const std::string& prefix) const
-    {
-    }
-    void checkPopulations(const std::string& name) const
-    {
-    }
-};
-
-template <typename StateInfo, typename ParentT, typename StateT, typename ChildT=void>
-class State
-{
-public:
-    using Parent = ParentT;
-    using ChildContainer = loki::experimental::ChildContainer<ChildT>;
-
-    const Parent& parent() const { return m_parent; }
-    const StateInfo& info() const { return m_info; }
-    // reconsider:
-    StateInfo& info() { return m_info; }
-    const ChildContainer& children() const { return m_children; }
-
-    ChildT* ensure_state(const std::string& info)
-    {
-        ChildT* c = m_children.find(info);
-        return c ? c : m_children.add(*static_cast<StateT*>(this),info);
-    }
-    template <class ...Args>
-    auto ensure_state(const std::string& info, Args... args)
-    {
-        return ensure_state(info)->ensure_state(args...);
-    }
-    bool operator==(const std::string& info) const
-    {
-        return m_info==info;
-    }
-    void print(std::ostream& os, const std::string& prefix=std::string{}) const
-    {
-        os << prefix << m_info.str() << std::endl;
-        m_children.print(os,prefix+' ');
-    }
-    State(const ParentT& parent, const std::string& name)
-    : m_parent(parent),
-      m_info(name)
-    {
-        static_assert(std::is_base_of_v<State,StateT>);
-    }
-    void checkPopulations() const
-    {
-        m_children.checkPopulations(m_info.str());
-    }
-private:
-    const Parent& m_parent;
-    ChildContainer m_children;
-    StateInfo m_info;
-};
-
-template <class StateT>
-const typename StateT::Parent::ChildContainer& siblings(const StateT& state)
-{
-    return state.parent().children();
-}
 
 class Gas;
 class StateRoot;
@@ -280,7 +291,7 @@ public:
     const Gas& getGas() const { return parent().getGas(); }
 };
 
-class StateRotational : public State<RotationalInfo,StateVibrational,StateRotational>
+class StateRotational : public State<RotationalInfo,StateVibrational,StateRotational,void>
 {
 public:
     using State::State;
