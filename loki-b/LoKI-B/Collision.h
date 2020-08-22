ad4104af5bad93b1031e2f9d155afe149e153a7d
jan <jan@linux-a9ex>
2020-08-22 21:14:37 +0200

    Store the stoichiometry in a map<State,uint16_t>
    Add members m_lhs and m_rhs of new type StoichArray to the Collision
    base class. This facilitates a precise comparison of processes. The
    equivalence if 'e + e + N2' and '2e + N2' and 'e + N2 + e' etc is now
    reliably detected.

--- loki-b/LoKI-B/Collision.h
+++ loki-b/LoKI-B/Collision.h
@@ -9,6 +9,7 @@
 #include "GasBase.h"
 
 #include <vector>
+#include <map>
 
 namespace loki {
 
@@ -27,7 +28,31 @@ namespace loki {
         using StateVector = std::vector<State*>;
         using CoeffVector = std::vector<uint16_t>;
 
-    protected:
+        using StoichArray = std::map<const State*,uint16_t>;
+        // Left-hand side-map of State pointers and stoichiometric coefficients
+        const StoichArray m_lhs;
+        // Right-hand side-map of State pointers and stoichiometric coefficients
+        const StoichArray m_rhs;
+
+        /** Compare the stoichiometry and type of the reaction.
+         */
+        bool is_same_as(const Collision& other) const
+        {
+            return type == other.type
+                && m_lhs == other.m_lhs
+                && m_rhs == other.m_rhs;
+        }
+        bool is_same_as(CollisionType other_type,
+                const StateVector& lhsStates,
+                const CoeffVector& lhsCoeffs,
+                const StateVector& rhsStates,
+                const CoeffVector& rhsCoeffs) const
+        {
+            return type == other_type
+                && m_lhs == makeStoichMap(lhsStates,lhsCoeffs)
+                && m_rhs == makeStoichMap(rhsStates,rhsCoeffs);
+        }
+protected:
 
         /** Initializes the members of the Collision class, the vectors are initialized
          *  using move semantics.
@@ -39,30 +64,24 @@ namespace loki {
                 const CoeffVector& rhsCoeffs,
                 bool isReverse)
         : type(type),
-        isReverse(isReverse)
-#if 0
-,
-        m_lhsStates(lhsStates),
-        m_lhsCoeffs(lhsCoeffs),
-        m_rhsStates(rhsStates),
-        m_rhsCoeffs(rhsCoeffs)
-#endif
+        isReverse(isReverse),
+        m_lhs(makeStoichMap(lhsStates,lhsCoeffs)),
+        m_rhs(makeStoichMap(rhsStates,rhsCoeffs))
         {
         }
-private:
-#if 0
-        // Vector of pointers to the reactant states.
-        const StateVector m_lhsStates;
-        // Vector of stoichiometric coefficents belonging to the reactants of the collsion.
-        const CoeffVector m_lhsCoeffs;
-
-        // Vector of pointers to the product states.
-        const StateVector m_rhsStates;
-        // Vector of stoichiometric coefficents belonging to the products of the collsion.
-        const CoeffVector m_rhsCoeffs;
-#endif
+        static StoichArray makeStoichMap(const StateVector& states, const CoeffVector& coeffs)
+        {
+            StoichArray sm;
+            assert(states.size()==coeffs.size());
+            for (unsigned i=0; i!=states.size(); ++i)
+            {
+                sm[states[i]] += coeffs[i];
+            }
+            return sm;
+        }
     };
-}
+
+} // namespace loki
 
 
 #endif //LOKI_CPP_COLLISION_H
