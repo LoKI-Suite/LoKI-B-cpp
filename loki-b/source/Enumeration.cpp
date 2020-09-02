6c1106aaef76444a971af3fd10a7f874f3a58e0b
jan <jan@linux-a9ex>
2020-09-02 14:15:21 +0200

    Added getCollisionTypeFromTypeTagArray.
    This accepts a json_type variable that supposedly represents a
    (collision) type_tags list. It returns a type id as expected by
    LoKI-B. Two things to notice:
    
     * when multiple tags are given, the 'least' precise is returned.
       At this moment only the combination "Electronic", "Vibrational"
       is handled, which selects "Electronic" (consistent with 'old'
       LoKI-B behaviour for the N2 input files: if an electron impact
       reaction results in electronic excitation AND in a vibrationsl
       change, the provess is an electronic state transition. Vibrational
       is used only for processes where*only* the vibrational state changes.
     * "Electronic" is translated in "Excitation", the name used by LoKI-B.

--- loki-b/source/Enumeration.cpp
+++ loki-b/source/Enumeration.cpp
@@ -7,6 +7,7 @@
  */
 
 #include "LoKI-B/Enumeration.h"
+#include "LoKI-B/Log.h"
 #include <vector>
 #include <utility>
 #include <stdexcept>
@@ -71,6 +72,51 @@ namespace {
                 { "Ionization", CollisionType::ionization },
                 { "Attachment", CollisionType::attachment },
                 });
+    }
+
+    /** \todo DB: How to get LoKI-B's type? There can be multiple tags, how do these map to LoKI-B's type?
+     *  Example: e + N2(X) -> e + N2(A3Su+,v=1-4) has type 'Excitation' in v4, but
+     *  type_tags "Electronic", "Vibrational" in v4.
+     */
+    CollisionType getCollisionTypeFromTypeTagArray(const json_type& type_tags)
+    {
+        const std::vector<std::string>& tags = type_tags;
+        if (tags.size()==0)
+        {
+            throw std::runtime_error("No type tags found.");
+        }
+        else if (tags.size()==1)
+        {
+            if (tags[0]=="Electronic")
+            {
+                Log<Message>::Notify("Found 'Electronic'. Setting the collision type to Excitation.");
+                return getCollisionType("Excitation");
+            }
+            else
+            {
+                return getCollisionType(tags[0]);
+            }
+        }
+        else
+        {
+            // for now: if Ionization is present, use that.
+            //            otherwise: if Electronic is present, use "Excitation" (translate).
+            // Otherwise we give up.
+            if (std::find(tags.begin(),tags.end(),"Ionization")!=tags.end())
+            {
+                Log<Message>::Notify("Found 'Ionization'. Setting the collision type to Ionization, ignoring other tags.");
+                return getCollisionType("Ionization");
+            }
+            else if (std::find(tags.begin(),tags.end(),"Electronic")!=tags.end())
+            {
+                Log<Message>::Notify("Found 'Electronic'. Setting the collision type to Excitation, ignoring other tags.");
+                return getCollisionType("Excitation");
+            }
+            else
+            {
+                throw std::runtime_error("type_tags: I do not know how to handle the type_tags array.");
+            }
+        }
     }
 
 } // namespace loki
