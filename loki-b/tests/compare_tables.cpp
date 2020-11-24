a714b1be95bd9cceabc1a7e416703c7e690c83e1
Jan van Dijk <jan@epgmod.phys.tue.nl>
2020-11-24 17:21:54 +0100

    Accept output filename argument. Show max_rel_diff

--- loki-b/tests/compare_tables.cpp
+++ loki-b/tests/compare_tables.cpp
@@ -13,7 +13,7 @@ loki::LookupTable extractLUT(std::istream& is)
     return lut;
 }
 
-void compareTables(const std::string& ifname1, const std::string& ifname2)
+double compareTables(const std::string& ifname1, const std::string& ifname2, const std::string& ofname)
 {
     std::ifstream ifs1{ifname1};
     if (!ifs1)
@@ -32,34 +32,48 @@ void compareTables(const std::string& ifname1, const std::string& ifname2)
     std::cout << "#Read loki header: " << header << std::endl;
     const loki::LookupTable lut_loki{extractLUT(ifs2)};
 
+    std::ofstream ofs{ofname};
+    if (!ofs)
+    {
+        throw std::runtime_error("Could not open file '" + ofname + "' for reading.");
+    }
     // now make a table based on the Bolsig+ energy points.
     // it prints the energy, the Bolsig+ table value and the loki table value
     // (note that the loki values will feature a table interpolation error when
     // a loki energy point is not also a Bolsig+ energy point).
+    double max_rel_diff = 0.0;
     for (loki::LookupTable::Vector::Index i=0; i!= lut_bolsig.size(); ++i)
     {
         const double e = lut_bolsig.x()[i];
-        std::cout << e << '\t' << lut_bolsig.interpolate(e) << '\t' << lut_loki.interpolate(e) << std::endl;
+        const double v1 = lut_bolsig.interpolate(e);
+        const double v2 = lut_loki.interpolate(e);
+        ofs << e << '\t' << v1 << '\t' << v2 << std::endl;
+        max_rel_diff = std::max(max_rel_diff,std::abs((v1-v2)/v1));
     }
+    std::cout << "Combined table written to file '" << ofname << "'." << std::endl;
+    return max_rel_diff;
 }
 
 const char* usage =
-    "Usage: compare_table <file1> <file2>.\n"
+    "Usage: compare_table <file1> <file2> <out-file>.\n"
     "\n"
-    "example: ./compare_table eedf_R4.out loki_eedf.out\n"
-    "will construct a lookup table from eedf_R4.out and compare the values\n"
-    "with those in loki_eedf.out. (The latter are interpolated.)\n";
+    "example: ./compare_table eedf_R4.out loki_eedf.out combined.out\n"
+    "will construct a lookup table from eedf_R4.out. It writes this lookup table\n"
+    "with xtra columns, representing the interpolated values of <file2> and the\n"
+    "relative differences.\n";
 
 int main(int argc, const char* argv[])
 {
     try {
-        if (argc!=3)
+        if (argc!=4)
         {
             throw std::runtime_error(usage);
         }
         const std::string ifname1(argv[1]);
         const std::string ifname2(argv[2]);
-        compareTables(ifname1,ifname2);
+        const std::string ofname(argv[3]);
+        const double max_rel_diff = compareTables(ifname1,ifname2,ofname);
+        std::cout << "# max_rel_diff: " << max_rel_diff << std::endl;
         return 0;
     }
     catch(std::exception& exc)
