742a8d25692222bc77e23d7aa794daf9e57461bf
Wouter Graef <wgraef@epgmod.phys.tue.nl>
2020-11-26 17:13:09 +0100

    Print results

--- testsuite/test.cpp
+++ testsuite/test.cpp
@@ -3,6 +3,27 @@
 #include "LoKI-B/Setup.h"
 #include "LoKI-B/Simulation.h"
 
+void printResults(const loki::Grid &grid,
+    const loki::Vector &eedf,
+    const loki::WorkingConditions &wc,
+    const loki::Power &power,
+    const std::vector<loki::EedfGas *> &gases,
+    const loki::SwarmParameters &swarmParameters,
+    const std::vector<loki::RateCoefficient> &rateCoefficients,
+    const std::vector<loki::RateCoefficient> &extraRateCoefficients,
+    const loki::Vector &firstAnisotropy)
+{
+    using namespace loki;
+    if (grid.getCells().size() != eedf.size())
+    {
+        throw std::runtime_error("error");
+    }
+    for (unsigned i = 0; i < grid.getCells().size(); i++)
+    {
+        std::cout << grid.getCells()[i] << "\t" << eedf[i] << std::endl;
+    }
+}
+
 int main(int argc, char **argv)
 {
     try
@@ -14,7 +35,7 @@ int main(int argc, char **argv)
         std::string fileName(argv[1]);
         const loki::json_type cnf = loki::read_json_from_file(fileName);
         std::unique_ptr<loki::Simulation> simulation(new loki::Simulation(cnf));
-//        simulation->m_obtainedResults.addListener(handleResults);
+        simulation->m_obtainedResults.addListener(printResults);
         simulation->run();
         return 0;
     }
