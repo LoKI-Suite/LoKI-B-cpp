bdae5601be4107d4135f33e71c8bcccdd7ae9c62
Jan van Dijk <jan@epgmod.phys.tue.nl>
2022-01-06 16:44:28 +0100

    See previous commit --- four files were forgotten.

---
+++ loki-b/LoKI-B/EedfMixture.h
@@ -0,0 +1,68 @@
+//
+// Created by daan on 21-5-19.
+//
+
+#ifndef LOKI_CPP_EEDFMIXTURE_H
+#define LOKI_CPP_EEDFMIXTURE_H
+
+#include "LoKI-B/GasMixture.h"
+#include "LoKI-B/EedfCollisions.h"
+
+#include <string>
+#include <vector>
+
+namespace loki
+{
+
+class EedfMixture
+{
+  public:
+    /** Initializes the gas mixture by loading the desired collisions from LXCat files.
+     *  These files are read from the electron kinetics setup structure. It also
+     *  requires a pointer to the energy grid in order to properly initialize the
+     *  cross sections of the collisions.
+     */
+    EedfMixture(Grid *grid, const ElectronKineticsSetup &setup, const WorkingConditions *workingConditions);
+    EedfMixture(Grid *grid, const json_type &cnf, const WorkingConditions *workingConditions);
+
+    const GasMixture& composition() const { return m_composition; }
+    GasMixture& composition() { return m_composition; }
+
+    const std::vector<const Gas *>& CARGases() const { return m_CARGases; }
+    const EedfCollisionDataMixture& collision_data() const { return m_collision_data; }
+    EedfCollisionDataMixture& collision_data() { return m_collision_data; }
+  private:
+    /** Loads the collisions from files, supplied through a vector of strings that hold
+     *  the filenames. Furthermore, it needs a pointer to the energy grid and a boolean to
+     *  indicate whether the collisions are extra, for correct initialization and storage of
+     *  the collisions. When the file extension is ".json", a JSON object is created from
+     *  the file and the handling of this file is delegated to member loadCollisionsJSON,
+     *  otherwise, the legacy LXCat file format is assumed and member loadCollisionsClassic
+     *  is called on the file.
+     */
+    void loadCollisions(const std::vector<std::string> &files, Grid *energyGrid, bool isExtra = false);
+
+    /** One extra property needs to be set from a file: OPBParameter.
+     *  This override sets this parameter and then calls Gas::loadGasProperties to set the
+     *  rest of its properties.
+     */
+    void loadGasProperties(const GasPropertiesSetup &setup);
+    /** One extra property needs to be set from a file: OPBParameter.
+     *  This override sets this parameter and then calls Gas::loadGasProperties to set the
+     *  rest of its properties.
+     */
+    void loadGasProperties(const json_type &cnf);
+
+    /// \todo comment addCARGas
+    void addCARGas(const std::string& gasName);
+
+    GasMixture m_composition;
+
+    EedfCollisionDataMixture m_collision_data;
+
+    std::vector<const Gas *> m_CARGases;
+};
+
+} // namespace loki
+
+#endif // LOKI_CPP_EEDFMIXTURE_H
