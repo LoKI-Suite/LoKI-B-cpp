ef27a80321440c997258d3ebb2490719c23723f1
Jan van Dijk <jan@epgmod.phys.tue.nl>
2022-08-30 20:25:45 +0200

    Add support file for dll imports/exports.

---
+++ LoKI-B/Exports.h
@@ -0,0 +1,22 @@
+#ifndef H_LOKI_B_EXPORTS_H
+#define H_LOKI_B_EXPORTS_H
+
+// For now we want to enable this only for MSVC builds
+// We could also add something like 'if doing a shared build',
+// at present we do that unconditionally.
+
+#ifdef _MSC_VER
+
+#define LOKIB_USE_DLL_IMPORTS
+
+#if defined(LOKIB_BUILDING_LIBLOKIB)
+#define lokib_export __declspec( dllexport )
+#elif defined(LOKIB_USE_DLL_IMPORTS)
+#define lokib_export __declspec( dllimport )
+#else
+#define lokib_export
+#endif
+
+#endif // _MSC_VER
+
+#endif // H_LOKI_B_EXPORTS_H
