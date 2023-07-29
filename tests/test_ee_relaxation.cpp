6c119732652981b13b5ba8eb19b0676b975e357f
Jan van Dijk <jan@epgmod.phys.tue.nl>
2023-07-29 23:05:24 +0200

    Add a demo that shows Maxwellization of an EEDF.
    The time-dependent Boltzmann equation is solved for a system of
    electrons that only undergo collisions among themselves. The
    program shows how the initial (Druyvesteyn) EEDF is Maxwellized
    as a result. The program demonstrates how the (conservative)
    formulation of the problem results in particle and energy
    conservation as time progresses.
    
    The program produces a file 'eedf.dat' with the results at given
    moments of time (separated by two empty lines). To show all eedfs
    with guplot, you may type (from a gnuplot window):
    
      set logscale y
      plot for [i=1:*] "eedf.dat" index i title sprintf("%d",i) with lines
    
    (The label is the write-index, not the time.)

---
+++ tests/test_ee_relaxation.cpp
@@ -0,0 +1,194 @@
+/** \file
+ *
+ *  A demonstration of class ElectronElectronOperator of the LoKI-B project.
+ *
+ *  LoKI-B solves a time and space independent form of the two-term
+ *  electron Boltzmann equation (EBE), for non-magnetised non-equilibrium
+ *  low-temperature plasmas excited by DC/HF electric fields from
+ *  different gases or gas mixtures.
+ *  Copyright (C) 2018-2022 A. Tejero-del-Caz, V. Guerra, D. Goncalves,
+ *  M. Lino da Silva, L. Marques, N. Pinhao, C. D. Pintassilgo and
+ *  L. L. Alves
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  Use class ElectronElectronOperator to solve the time-dependent EEDF for
+ *  a system of electrons that only undergo collisions among themselves.
+ *  The program shows the resulting Maxwellization of the the initial
+ *  (Druyvesteyn) distribution.
+ *
+ *  This program is run without arguments. See the top of function main() for
+ *  the precise simulation settings.
+ *  The program produces a file 'eedf.dat' in the current working directory
+ *  that consists of blocks that contain f(u). A block is written after every
+ *  100 time steps, its header reveals some more information about the state
+ *  of affairs.
+ *
+ *  Note that the EEDF is *not* normalized during the simulation. Since a
+ *  conservative formulation is used, the EEDF will stay normalized as time
+ *  progresses. This also means that the Boltzmann equation is solved for *all*
+ *  grid points (in an iterative method, normalisation can be enforced  by
+ *  replacing the equation for one grid point by the normalization condition
+ *  before solving the system). The time-discretization uses the backward Euler
+ *  approximation.
+ *
+ *  \author Jan van Dijk
+ *  \date   July 2023
+ */
+
+#include "LoKI-B/Grid.h"
+#include "LoKI-B/Operators.h"
+#include "LoKI-B/EedfUtilities.h"
+#include <iostream>
+#include <fstream>
+
+#include <Eigen/SparseLU>
+#include <Eigen/OrderingMethods>
+
+void write_eedf(std::ostream& os, const loki::Grid& grid, const loki::Vector& eedf, unsigned iter, double t, double TeV, double norm, double maxRelDiff, double next_dt, int info)
+{
+    if (iter != 0)
+    {
+        os << std::endl << std::endl;
+    }
+    os << "# " << "iteration  = " << iter << std::endl;
+    os << "# " << "t          = " << t << std::endl;
+    os << "# " << "TeV        = " << TeV << std::endl;
+    os << "# " << "norm       = " << norm << std::endl;
+    os << "# " << "maxRelDiff = " << maxRelDiff << std::endl;
+    os << "# " << "next_dt     = " << next_dt << std::endl;
+    os << "# " << "info       = " << info << std::endl;
+    for (loki::Grid::Index k = 0; k < grid.nCells(); ++k)
+    {
+        os << grid.getCells()(k) << '\t' << eedf(k) << std::endl;
+    }
+}
+
+int main(int argc, const char* argv[])
+{
+    using namespace loki;
+
+    const unsigned nCells = 64;
+    const double uMax = 4; // eV
+    const double ne = 1e16; // m^-3
+    const double n0 = 1e22; // m^-3
+
+    const double shape = 2.0; // 1: Maxwell, 2: Druyvesteyn
+
+    const Grid grid(nCells,uMax);
+    ElectronElectronOperator eeOperator;
+    eeOperator.initialize(grid);
+    eeOperator.updateABMatrices(grid);
+
+    SparseMatrix M(grid.nCells(),grid.nCells());
+    Vector rhs = Vector::Zero(grid.nCells());
+
+    const double t_max = 1.0; // s
+    const unsigned iter_write = 100;
+
+    double t=0.0; // s (initial value)
+    // the initial eedf.
+    double TeV = 1.0; // requested value, in eV
+    Vector eedf = makePrescribedEDF(grid,shape,TeV);
+    /* We know that makePrescribedEDF normalizes the matrix, do not recalculate 'norm'.
+     * Note that at this stage 2*<u>/3 will *not* be equal to TeV exactly, because of
+     * discretisation effects. The EEDF is normalized to make the cumulative chance 1,
+     * the mean energy will not be reproduced exactly. The mismatch is smaller for
+     * finer meshes. So recalculate TeV, ensure the actual value is written in the
+     * upcoming call to write_eedf.
+     */
+    double norm=1.0;
+    TeV = 2. / 3. * getMeanEnergy(eedf,grid);
+
+    double dt=1e-10; // s (initial value)
+    unsigned iter=0;
+
+    std::ofstream ofs("eedf.dat");
+    write_eedf(ofs,grid,eedf,iter,t,TeV,norm, 0, dt, 0);
+    while (t<t_max)
+    {
+        // part 1. Update the target time and the ee-operator data.
+        t += dt;
+        eeOperator.update_g_ee(grid,eedf,ne,n0);
+        TeV = 2. / 3. * getMeanEnergy(eedf,grid);
+        eeOperator.updateAB(grid,eedf);
+
+        /* NOTE: the equation is discretized as in LoKI-B, as:
+         *
+         *    -(sqrt(u)/(N*gamma)*df/dt - 1/(N*gamma)*dG/du = 0
+         *
+         * That explains all the minus signs.
+         */
+        for (Grid::Index k = 0; k < grid.nCells(); ++k)
+        {
+            /* part 1: - 1/(N*gamma)*dG/du. This code is the same as that in
+             * source/Operators.cpp. This *sets* the elements of matrix M.
+             */
+            M.coeffRef(k, k) = - (eeOperator.A[k] + eeOperator.B[k]);
+
+            if (k > 0)
+                M.coeffRef(k, k - 1) = + eeOperator.A[k - 1];
+
+            if (k < grid.nCells() - 1)
+                M.coeffRef(k, k + 1) = + eeOperator.B[k + 1];
+
+            /* part 2: - sqrt(u)/(N*gamma)*df/dt. We use df/dt .= (f-f_old)/dt):
+             * the first terms leads to an *additional* contribution to M, the
+             * second appears as an inhomogemeous term on the RHS.
+             */
+            const double cf=std::sqrt(grid.getCells()(k))/(n0*SI::gamma);
+            M.coeffRef(k,k) -= cf/dt;
+            rhs(k) = -cf*eedf(k)/dt;
+        }
+
+        ///
+
+        /// \todo Can we avoid this? Only needed to call makeCompressed
+        SparseMatrix tmp(M);
+        /// \todo necessary? It seems that the copy ctor will make the target compressed.
+        tmp.makeCompressed();
+        Eigen::SparseLU<SparseMatrix,Eigen::AMDOrdering<typename SparseMatrix::StorageIndex>> solver;
+        solver.compute(tmp);
+        const Vector eedf_old = eedf;
+        eedf = solver.solve(rhs);
+
+        /* Calculate the normalization status of the EEDF. Ideally this should stay 1,
+         * since the algorithm is conservative and no particles should disappear, up
+         * to the order of the machine accuracy.
+         */
+        norm = eedf.dot(grid.getCells().cwiseSqrt() * grid.du());
+        /* Calculate the highest relative change of the eedf in any of the grid points.
+         */
+        double maxRelDiff=0.0;
+        for (Grid::Index k = 0; k < grid.nCells(); ++k)
+        {
+            const double localRelDiff = std::abs( (eedf(k)-eedf_old(k))/eedf_old(k) );
+            maxRelDiff=std::max(maxRelDiff,localRelDiff);
+        }
+        /* Use that change to adjust the next timestep.
+         */
+        const double tolRelDiff=1e-3;
+        double dtSuggested = dt*tolRelDiff/maxRelDiff;
+        double dtClamped = std::min(2*dt,dtSuggested);
+        dt = dtClamped;
+        ++iter;
+
+        // write the EEDF every iter_write time steps
+        if (iter%iter_write==0)
+        {
+                write_eedf(ofs,grid,eedf,iter,t,TeV,norm,maxRelDiff,dt,solver.info());
+        }
+    }
+    return 0;
+}
