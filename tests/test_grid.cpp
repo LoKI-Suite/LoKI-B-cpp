b15a6e37eddd67efda3454259db93b0c0e60bfc6
Jan van Dijk <jan@epgmod.phys.tue.nl>
2023-09-17 00:01:08 +0200

    Update date, add a comment.

--- tests/test_grid.cpp
+++ tests/test_grid.cpp
@@ -1,8 +1,9 @@
 /** \file
+ *
  *  Unit tests for Grid.h
  *
  *  \author Jan van Dijk
- *  \date   November 2020
+ *  \date   September 2023
  */
 
 #include "LoKI-B/Grid.h"
@@ -22,7 +23,7 @@ int main()
     test_expr( std::abs(grid1.duNodes().sum()-grid1.uMax()) < eps*grid1.uMax() );
 
     Grid::Vector ls(6); ls << 0.0,0.2,0.4,0.6,0.8,1.0;
-    Grid grid2(ls,10,true);
+    Grid grid2(ls,10,true); // true: we promise that ls is indeed equidistant
     test_expr( std::abs(grid2.duCells().sum()-grid2.uMax()) < eps*grid2.uMax() );
     test_expr( std::abs(grid2.duNodes().sum()-grid2.uMax()) < eps*grid2.uMax() );
 
