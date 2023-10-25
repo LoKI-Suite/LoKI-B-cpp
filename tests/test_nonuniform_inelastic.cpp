3b354b69e4bb3674d33aaea931bfb6efe754f0b4
JopHendrikx <j.hendrikx@student.tue.nl>
2023-10-25 12:29:34 +0200

    Test uniform grid on inelastic delta peak.
    Uniform grid is constructed and tested to analytical solution.
    Add input file.

--- tests/test_nonuniform_inelastic.cpp
+++ tests/test_nonuniform_inelastic.cpp
@@ -9,286 +9,120 @@
 #include "LoKI-B/Grid.h"
 #include "LoKI-B/EedfUtilities.h"
 #include "LoKI-B/EedfCollisions.h"
+#include "LoKI-B/Simulation.h"
 #include "tests/TestUtilities.h"
 #include "source/Operators.cpp"
 #include <iostream>
 #include <fstream>
 
-auto json = R"json(
-{
-    "mixture": {
-        "states": {
-            "e": {
-                "particle": "e",
-                "charge": -1
-            },
-            "X(0)": {
-                "particle": "X",
-                "charge": 0,
-                "electronic": [
-                    {
-                        "e": "0",
-                        "summary": "0"
-                    }
-                ]
-            },
-            "X(*)": {
-                "particle": "X",
-                "charge": 0,
-                "electronic": [
-                    {
-                        "e": "*",
-                        "summary": "*"
-                    }
-                ]
-            },
-            "X(+)": {
-                "particle": "X",
-                "charge": 1,
-                "electronic": [
-                    {
-                        "e": "0",
-                        "summary": "0"
-                    }
-                ]
-            }
-        },
-        "processes": [
-            {
-                "reaction": {
-                    "lhs": [
-                        { "state": "e", "count": 1 },
-                        { "state": "X(0)", "count": 1 }
-                ],
-                "rhs": [
-                    { "state": "e", "count": 1 },
-                    { "state": "X(0)", "count": 1 }
-                ],
-                "reversible": false,
-                "type_tags": ["Excitation"]
-                },
-                "parameters": ["m/M = 0.000025"],
-                "reference": [
-                    "Analytical test, constant cross section"
-                ],
-                "labels": ["Energy", "Cross section"],
-                "units": ["eV", "m2"],
-                "threshold": 9.99,
-                "data": [
-                    [9.990000e+0, 0],
-                    [10.0, 1e6],
-                    [10.010000e+0, 0]
-                ]
-            },
-            {
-                "reaction": {
-                    "lhs": [
-                        { "state": "e", "count": 1 },
-                        { "state": "X(0)", "count": 1 }
-                ],
-                "rhs": [
-                    { "state": "e", "count": 1 },
-                    { "state": "X(0)", "count": 1 }
-                ],
-                "reversible": false,
-                "type_tags": ["Elastic"]
-                },
-                "parameters": ["m/M = 0.00025"],
-                "reference": [
-                    "Analytical test, constant cross section"
-                ],
-                "labels": ["Energy", "Cross section"],
-                "units": ["eV", "m2"],
-                "threshold": 0,
-                "data": [
-                    [-0.1,0],
-                    [4.99,0], 
-                    [5.0, 0.2],
-                    [10.0, 0.2]
-                ]
-            },
-            {
-                "reaction": {
-                    "lhs": [
-                        { "state": "e", "count": 1 },
-                        { "state": "X(0)", "count": 1 }
-                ],
-                "rhs": [
-                    { "state": "e", "count": 1 },
-                    { "state": "X(0)", "count": 1 }
-                ],
-                "reversible": false,
-                "type_tags": ["Elastic"]
-                },
-                "parameters": ["m/M = 0.00025"],
-                "reference": [
-                    "Analytical test, constant cross section"
-                ],
-                "labels": ["Energy", "Cross section"],
-                "units": ["eV", "m2"],
-                "threshold": 0.98,
-                "data": [
-                    [0.98,0],
-                    [0.9990000e+0, 1e25],
-                    [1.100000e+0, 1e25],
-                    [1.11, 0]
-                ]
-            }  
-        ]
-    },
-    "gasProperties": {
-        "_mass": "test_masses.txt",
-        "mass": [
-            { "name": "X", "mass": 1 }
-        ],
-        "fraction": [
-            "X = 1"
-        ],
-        "_harmonicFrequency": "test_harmonicFrequency.txt",
-        "harmonicFrequency": [
-            { "name": "X", "harmonicFrequency": 1 }
-        ],
-        "_anharmonicFrequency": "test_anharmonicFrequency.txt",
-        "anharmonicFrequency": [
-            { "name": "X", "anharmonicFrequency": 1 }
-        ],
-        "_electricQuadrupoleMoment": "test_electricQuadrupoleMoment.txt",
-        "electricQuadrupoleMoment": [
-            { "name": "X", "electricQuadrupoleMoment": 1 }
-        ],
-        "_rotationalConstant": "test_RotationalConstant.txt",
-        "rotationalConstant": [
-            { "name": "X", "rotationalConstant": 1 }
-        ],
-        "_OPBParameter": "test_OPBParameter.txt",
-        "OPBParameter": [
-            { "name": "X", "OPBParameter": 1 }
-        ]
-    },
-    "stateProperties": {
-        "energy": [
-            "X(0) = 0.0"
-        ],
-        "population": [
-            "X(0) = 1.0"
-        ],
-        "statisticalWeight": [
-            "X(0) = 1"
-        ]
-    }
-    
-}
-)json"_json;
-
 void write_eedf(std::ostream& os, const loki::Grid& grid, const loki::Vector& eedf)
 {
     for (loki::Grid::Index k = 0; k < grid.nCells(); ++k)
     {
-        os << grid.getCells()(k) << '\t' << eedf(k) << std::endl;
+        os << grid.getCells()(k) << '\t' << eedf(k)  << std::endl;
     }
 }
 
-int main()
+void write_eedfAnalytical(std::ostream& os, const loki::Grid& grid, const loki::Vector& eedf, double e)
+{
+    for (loki::Grid::Index k = 0; k < grid.nCells(); ++k)
+    {
+        os << grid.getCells()(k)/e << '\t' << eedf(k)<< std::endl;
+    }
+}
+
+loki::Vector analyticalSolution()
 {
     using namespace loki;
-    
-    int nCells = 3000;
-    double uMax = 30;
-    double Tg = 00;
-    const double eon = 10;
-    const double won = 0;
+    double Td = 1e-21;
+    double e = Constant::electronCharge;
+
+    double uMax = 30.*e;
+    int nCells = 6000;
+    const double eon = 10*Td;
+
+    double u0 = 10.*e;
+    double sigma0 = 1e-21;
+    double Q = 1e-19;
+    double mM = 2.5e-5;
+    double W = e/ std::sqrt(6*mM) * eon/Q;
 
     Grid::Vector nodeDistribution = Vector::LinSpaced(nCells + 1, 0.0, 1.0);
     Grid grid1(nodeDistribution, uMax, false);
     Grid grid2(nCells, uMax);
-
-    ElectronKineticsSetup kineticSetup;
-    WorkingConditionsSetup conditionsSetup;
-    conditionsSetup.gasTemperature = Tg;
-    WorkingConditions workConditions(conditionsSetup);
-    
-    EedfMixture mixture1(&grid1, json, &workConditions);
-    EedfMixture mixture2(&grid2, json, &workConditions);
-    
-    InelasticOperator inelasticOperator1(grid1);
-    InelasticOperator inelasticOperator2(grid2);
-   
-    SparseMatrix M1(nCells, nCells);
-    Matrix boltzmann1 = M1.toDense(); 
-
-    SparseMatrix M2(nCells, nCells);
-    Matrix boltzmann2 = M2.toDense(); 
-
-    
-    inelasticOperator2.evaluateInelasticOperators(grid2,mixture2);
-    inelasticOperator1.evaluateInelasticOperators(grid1,mixture1);
-    test_expr(inelasticOperator1.inelasticMatrix.isApprox(inelasticOperator2.inelasticMatrix));
-
-
-    // Elastic part
-    Vector elasticCrossSection = Vector::Ones(nCells+1);
-    ElasticOperator elasticOperator;
-    SparseMatrix Melastic1(nCells, nCells);
-    SparseMatrix Melastic2(nCells, nCells);
-    
-    elasticOperator.evaluate(grid1, elasticCrossSection, Tg, Melastic1);
-    elasticOperator.evaluate(grid2, elasticCrossSection, Tg, Melastic2);
-    test_expr(Melastic1.isApprox(Melastic2));
-    
-    // Field part
-    Vector fieldCrossSection = Vector::Ones(nCells+1);
-    FieldOperator fieldOperator1(grid1);
-    FieldOperator fieldOperator2(grid2);
-    SparseMatrix Mfield1(nCells, nCells);
-    SparseMatrix Mfield2(nCells, nCells);
-    fieldOperator1.evaluate(grid1, fieldCrossSection, eon, won, Mfield1);
-    fieldOperator2.evaluate(grid2, fieldCrossSection, eon, won, Mfield2);
-
-    boltzmann1 = inelasticOperator1.inelasticMatrix + Melastic1 + Mfield1;
-    Vector eedf1 = Vector::Zero(nCells);
-    eedf1[0] = 1.;
-
-    boltzmann1.row(0) = grid1.getCells().cwiseSqrt().cwiseProduct(grid1.duCells());
-    LinAlg::hessenberg(boltzmann1.data(), eedf1.data(), grid1.nCells());
-
-
     Vector f = Vector::Zero(nCells);
+    Vector fhg = Vector::Zero(nCells);
     Vector fIhg = Vector::Zero(nCells);
-
-    double refU = 0;
-    double sigma = 1e-1;
-    double Q0 = 1e-0;
-    // double mM = 2.5e-5;
-    // double e = Constant::electronCharge;
-    double u0 = 10;
-    // double ureac = 10;
-    // double W = e/ std::sqrt(6*mM) * eon/Q0;
 
     Grid::Index findIndex = (std::upper_bound(grid1.getCells().begin(),grid1.getCells().end(), u0) - grid1.getCells().begin());
     
     for (Grid::Index k = 0; k < grid1.nCells(); ++k)
         {
             // Analytical solution
-            f[k] = std::exp(-3*Q0*(std::pow(grid1.getCell(k),2) - std::pow(refU,2)) /std::pow(eon,2));
-            fIhg[k] = 6*sigma / (std::pow(eon,2) * Q0) * std::exp(-3*std::pow(Q0 * refU /eon,2))*(+std::expint(3*std::pow(Q0*grid1.getCell(k)/eon,2)) - std::expint(3*std::pow(Q0*u0/eon,2)));
-
-            // Solution from Peter Koelman 
-            // f[k] = std::exp(-1*std::pow(u0/W,2) / 2 * (std::pow(grid1.getCell(k)/u0,2) - std::pow(refU/u0,2)));
-            // fihg[k] = sigma / 4 / mM / Q0 * std::pow(ureac/W,2) * std::exp(-std::pow(ureac/W,2)/2)*(std::expint(std::pow(grid1.getCell(k)/W,2)/2) - std::expint(std::pow(ureac/W,2)/2));
-            if (k > findIndex)
+            fhg[k] = std::exp(-1*std::pow(u0/W,2)/2 * (std::pow(grid1.getCell(k)/u0,2) - std::pow(u0/u0,2)));
+            
+            
+            if (k <= findIndex)
             {
-                f[k] *= (1+fIhg[k]);
+                fIhg[k] = 1/(mM * 4)*sigma0/Q * std::pow(u0/W, 2)*std::exp(-std::pow(u0/W,2)/2)*(-std::expint(std::pow(grid1.getCell(k)/W,2) / 2) + std::expint(std::pow(u0/W,2) / 2));
+                f[k] = fhg[k]*(1+fIhg[k]);
 
-            } 
+            } else
+            {
+                f[k] = fhg[k];
+            }
             
         }
     f /= f.dot(grid1.getCells().cwiseSqrt().cwiseProduct(grid1.duCells())); 
-    
-    double relativeError = ((eedf1-f).cwiseQuotient(f).cwiseAbs()).mean();
-    std::cout << relativeError << std::endl;
+    f *= std::pow(e,1.5); 
+    std::ofstream ofs1("eedfinelasticAnalytical.dat");
+    write_eedfAnalytical(ofs1,grid2,f,e);
+    return f;
+}
 
+void checkRMSE(const loki::Grid &grid,
+    const loki::Vector &eedf,
+    const loki::WorkingConditions &wc,
+    const loki::Power &power,
+    const loki::EedfCollisionDataMixture& gases,
+    const loki::SwarmParameters &swarmParameters,
+    const loki::Vector *firstAnisotropy
+)
+{
+    using namespace loki;
+    if (grid.getCells().size() != eedf.size())
+    {
+        throw std::runtime_error("error");
+    }
     std::ofstream ofs("eedfinelastic.dat");
-    write_eedf(ofs,grid1,eedf1);
+    write_eedf(ofs,grid,eedf);
+
+    Vector solution = analyticalSolution();
+    Vector relativeError = ((solution - eedf).cwiseQuotient(solution).cwiseAbs());
+    std::cerr << relativeError.mean() << std::endl;
+    std::ofstream ofs2("eedferror.dat");
+    write_eedf(ofs2,grid,relativeError);
+}
+
+
+int main()
+{
+    using namespace loki;
+
+    try
+    {
+        std::ifstream ifs("input/JSON/NonUniform/delta.input.json");
+        auto j = nlohmann::json::parse(ifs);
+
+        std::unique_ptr<loki::Simulation> simulation(new loki::Simulation(j));
+        simulation->m_obtainedResults.addListener(checkRMSE);
+        simulation->run();
+    }
+    catch (const std::exception &exc)
+    {
+        std::cerr << exc.what() << std::endl;
+        return 1;
+    }
 
     test_report;
     return nerrors;
