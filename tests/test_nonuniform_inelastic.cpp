c0f657d8cc34ee5c1d19e7e2f2ec70d44928ffa9
JopHendrikx <j.hendrikx@student.tue.nl>
2023-10-27 17:08:19 +0200

    Add analytical function
    Also compares numerical result with analytical function.

--- tests/test_nonuniform_inelastic.cpp
+++ tests/test_nonuniform_inelastic.cpp
@@ -22,6 +22,13 @@ void write_eedf(std::ostream& os, const loki::Grid& grid, const loki::Vector& ee
         os << grid.getCells()(k) << '\t' << eedf(k)  << std::endl;
     }
 }
+void write_grid(std::ostream& os, const loki::Vector nodeDistribution, const loki::Vector& eedf)
+{
+    for (loki::Grid::Index k = 0; k < nodeDistribution.size(); ++k)
+    {
+        os << nodeDistribution[k] << '\t' << eedf(k)  << std::endl;
+    }
+}
 
 void write_eedfAnalytical(std::ostream& os, const loki::Grid& grid, const loki::Vector& eedf, double e)
 {
@@ -31,14 +38,14 @@ void write_eedfAnalytical(std::ostream& os, const loki::Grid& grid, const loki::
     }
 }
 
-loki::Vector analyticalSolution()
+loki::Vector analyticalSolution(loki::Grid const &grid)
 {
     using namespace loki;
     double Td = 1e-21;
     double e = Constant::electronCharge;
 
     double uMax = 30.*e;
-    int nCells = 6000;
+    int nCells = grid.nCells();
     const double eon = 10*Td;
 
     double u0 = 10.*e;
@@ -47,9 +54,11 @@ loki::Vector analyticalSolution()
     double mM = 2.5e-5;
     double W = e/ std::sqrt(6*mM) * eon/Q;
 
-    Grid::Vector nodeDistribution = Vector::LinSpaced(nCells + 1, 0.0, 1.0);
+    Grid::Vector nodeDistribution = grid.getNodes()/grid.uMax();
+    
     Grid grid1(nodeDistribution, uMax, false);
-    Grid grid2(nCells, uMax);
+    std::ofstream ofs("grid1.dat");
+    write_grid(ofs,grid1.getNodes()/e,Vector::Ones(nCells+1));
     Vector f = Vector::Zero(nCells);
     Vector fhg = Vector::Zero(nCells);
     Vector fIhg = Vector::Zero(nCells);
@@ -62,7 +71,7 @@ loki::Vector analyticalSolution()
             fhg[k] = std::exp(-1*std::pow(u0/W,2)/2 * (std::pow(grid1.getCell(k)/u0,2) - std::pow(u0/u0,2)));
             
             
-            if (k <= findIndex)
+            if (k < findIndex)
             {
                 fIhg[k] = 1/(mM * 4)*sigma0/Q * std::pow(u0/W, 2)*std::exp(-std::pow(u0/W,2)/2)*(-std::expint(std::pow(grid1.getCell(k)/W,2) / 2) + std::expint(std::pow(u0/W,2) / 2));
                 f[k] = fhg[k]*(1+fIhg[k]);
@@ -76,7 +85,7 @@ loki::Vector analyticalSolution()
     f /= f.dot(grid1.getCells().cwiseSqrt().cwiseProduct(grid1.duCells())); 
     f *= std::pow(e,1.5); 
     std::ofstream ofs1("eedfinelasticAnalytical.dat");
-    write_eedfAnalytical(ofs1,grid2,f,e);
+    write_eedfAnalytical(ofs1,grid1,f,e);
     return f;
 }
 
@@ -90,20 +99,20 @@ void checkRMSE(const loki::Grid &grid,
 )
 {
     using namespace loki;
+    loki::Vector solution = analyticalSolution(grid);
+    
     if (grid.getCells().size() != eedf.size())
     {
         throw std::runtime_error("error");
     }
     std::ofstream ofs("eedfinelastic.dat");
     write_eedf(ofs,grid,eedf);
-
-    Vector solution = analyticalSolution();
+    
     Vector relativeError = ((solution - eedf).cwiseQuotient(solution).cwiseAbs());
     std::cerr << relativeError.mean() << std::endl;
     std::ofstream ofs2("eedferror.dat");
     write_eedf(ofs2,grid,relativeError);
 }
-
 
 int main()
 {
@@ -111,8 +120,40 @@ int main()
 
     try
     {
-        std::ifstream ifs("input/JSON/NonUniform/delta.input.json");
+        int n = 600;
+        int n1= 200;
+        
+        int nCells =  n + n1;
+        double U0 = 10.;
+        double duPeak = 1e-1;
+        double Umax = 30.;
+        double deltau = 2*duPeak/n1;
+        double sigma0 = 1e-21;
+        std::ifstream ifs("input/JSON/NonUniform/nonuniformDelta.input.json");
         auto j = nlohmann::json::parse(ifs);
+        Vector part1 = Vector::LinSpaced(n*U0/Umax, 0.0/Umax, (U0 - duPeak - 0.1*duPeak)/Umax);
+        Vector part2 = Vector::LinSpaced(n1 + 1, (U0 - duPeak)/Umax, (U0 + duPeak)/Umax);
+        Vector part3 = Vector::LinSpaced(n*(Umax - U0)/Umax, (U0 + duPeak + 0.1*duPeak)/Umax, Umax/Umax);
+
+        Vector total(nCells +1);
+        total << part1, part2, part3;
+        std::sort(total.begin(), total.end());
+        std::ofstream ofs("grid.dat");
+        write_grid(ofs,total,Vector::Ones(nCells+1));
+        j["electronKinetics"]["numerics"]["energyGrid"]["nonuniformGrid"]["nodeDistribution"] = total; 
+        j["electronKinetics"]["numerics"]["energyGrid"]["nonuniformGrid"]["maxEnergy"] = Umax;
+
+        j["electronKinetics"]["mixture"]["processes"][1]["threshold"] = U0 - deltau;
+        j["electronKinetics"]["mixture"]["processes"][1]["data"][0][0]  = 0;
+        j["electronKinetics"]["mixture"]["processes"][1]["data"][1][0]  = U0 - deltau;
+        j["electronKinetics"]["mixture"]["processes"][1]["data"][2][0]  = U0;
+        j["electronKinetics"]["mixture"]["processes"][1]["data"][3][0]  = U0 + deltau;
+        j["electronKinetics"]["mixture"]["processes"][1]["data"][4][0]  = 1e3;
+        j["electronKinetics"]["mixture"]["processes"][1]["data"][0][1]  = 0;
+        j["electronKinetics"]["mixture"]["processes"][1]["data"][1][1]  = 0;
+        j["electronKinetics"]["mixture"]["processes"][1]["data"][2][1]  = sigma0*U0/(deltau);
+        j["electronKinetics"]["mixture"]["processes"][1]["data"][3][1]  = 0;
+        j["electronKinetics"]["mixture"]["processes"][1]["data"][4][1]  = 0;
 
         std::unique_ptr<loki::Simulation> simulation(new loki::Simulation(j));
         simulation->m_obtainedResults.addListener(checkRMSE);
