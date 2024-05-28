2ac9c1b2a1ce414faabd8d1041c593fb1578385d
Daan Boer <daan-boer@hotmail.com>
2024-05-28 11:44:26 +0200

    Throw an error when a double process is encountered

--- source/EedfCollisions.cpp
+++ source/EedfCollisions.cpp
@@ -750,12 +750,13 @@ EedfCollision *EedfCollisionDataMixture::addCollision(CollisionType type, const 
     {
         if (c.m_coll->is_same_as(*coll_uptr))
         {
-            Log<DoubleCollision>::Warning(*c.m_coll);
 //#define ALLOW_DUPLICATE_PROCESSES
 #ifdef ALLOW_DUPLICATE_PROCESSES
             // duplicates are allowed. The warning is emitted,
             // but no further action needed
+            Log<DoubleCollision>::Warning(*c.m_coll);
 #else
+            Log<DoubleCollision>::Error(*c.m_coll);
             return nullptr;
 #endif
         }
